0b4c63c94fe34376f6ba24ba072a396a6b01ca4b
Manuel Lerchner <manuel.lerchner@quantco.com>
2026-03-03 17:33:54 +0100

    remove all headers

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,37 +13,15 @@ from tests.fixtures.mock_httpx_client import mock_httpx_client
 CASSETTE_DIR = Path(__file__).parent / "cassettes"
 
 
-FILTERED_HEADERS = [
-    "authorization",
-    "Authorization",
-    "X-TFS-Session",
-    "X-TFS-ProcessId",
-    "X-VSS-E2EID",
-    "X-VSS-SenderDeploymentId",
-    "X-GitHub-Api-Version",
-    "x-ratelimit-limit",
-    "x-ratelimit-remaining",
-    "x-ratelimit-reset",
-    "x-ratelimit-used",
-    "x-ratelimit-resource",
-    "x-github-request-id",
-    "x-github-media-type",
-    "x-oauth-scopes",
-    "x-accepted-oauth-scopes",
-    "cookie",
-    "set-cookie",
-]
+def _scrub_request_headers(request):
+    """Remove all request headers."""
+    request.headers = {}
+    return request
 
 
 def _scrub_response_headers(response):
-    """Remove sensitive headers from response."""
-    headers_to_remove = [
-        h
-        for h in response["headers"]
-        if h.lower() in [f.lower() for f in FILTERED_HEADERS]
-    ]
-    for header in headers_to_remove:
-        del response["headers"][header]
+    """Remove all response headers."""
+    response["headers"] = {}
     return response
 
 
@@ -51,9 +29,9 @@ def _scrub_response_headers(response):
 def vcr_config():
     return {
         "cassette_library_dir": str(CASSETTE_DIR),
-        "filter_headers": FILTERED_HEADERS,
         "record_mode": "once",
         "match_on": ["method", "scheme", "host", "port", "path", "query"],
+        "before_record_request": _scrub_request_headers,
         "before_record_response": _scrub_response_headers,
         "decode_compressed_response": True,
     }
